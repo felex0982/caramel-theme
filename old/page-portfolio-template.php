bd3be2a48e35dd2bfa2a108bc00e8afbc153c96b
Felix <moin@fawagner.de>
2020-07-21 13:05:07 +0200

    add template files

---
+++ old/page-portfolio-template.php
@@ -0,0 +1,40 @@
+<?php
+/*
+	Template Name: Portfolio Template
+*/
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+Portfolio Template
+
+
+    <?php
+    the_title();
+
+    $args = array('post_type' => 'portfolio');
+    $loop = new WP_Query($args);
+
+	if ( $loop->have_posts() ) {
+        ?>
+        <div class="masonry-grid"> 
+            <?php
+            while ( $loop->have_posts() ): $loop->the_post(); ?>
+                <div class="masonry-grid__item">
+                    <?php
+                        get_template_part( 'template-parts/content', get_post_type() );
+                    ?>
+                </div>
+            <?php endwhile;
+            ?>
+        </div>
+        <?php
+	}
+	?>
+
+
+</main><!-- #site-content -->
+
+<?php
+get_footer();
