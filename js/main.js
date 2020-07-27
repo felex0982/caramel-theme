128d52f7c16c151edba652a08796e2aac30b0d04
Felix <felix@bottlebase.com>
2020-07-27 17:15:02 +0200

    start caramel slider

--- js/main.js
+++ js/main.js
@@ -3,8 +3,20 @@
 console.log("------ Welcome to Caramel https://caramel.flellex.de ------");
 
 class CaramelSlider {
+    // For now only one Slider per Page
+    slider;
+    slides;
+    slideIndex = 1;
+
     constructor() {
-        this.slides = document.querySelectorAll('.caramel-slides');
+        this.slider = document.querySelector('#caramel-slider');
+        this.slides = this.slider.querySelectorAll('.caramel-slide');
+        this.slider.querySelector('#caramel-slider-next').addEventListener('click', nextSlide());
+
+        this.slides.forEach(slide => console.log(slide));
     }
+
 }
+
+new CaramelSlider();
 // End - Main Image Slider
--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -13,36 +13,36 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 
 ?>
-<div class="caramel-slider">
-    <?php
-        $query = new WP_query(
-            array(
-                'post_type' => array('sliderimage'),
-                'post_status' => 'publish',
-            )
-        );
-        $i = 1;
-        foreach ($query->posts as $post) {
-            ?>
-            <div class="caramel-slide">
-                <div class="caramel-slide__number"><?php echo($i); ?></div>
-                <?php
-                    the_post_thumbnail('', array('class' => 'caramel-slide__image'));
+<div id="caramel-slider" class="caramel-slider">
+    <div class="caramel-slider__content">
+        <?php
+            $query = new WP_query(
+                array(
+                    'post_type' => array('sliderimage'),
+                    'post_status' => 'publish',
+                )
+            );
+            $i = 1;
+            foreach ($query->posts as $post) {
                 ?>
-                <div class="caramel-slide__text"><?php echo(get_the_title($post)); ?></div>
-            </div>
-            <?php
-            $i++;
-        }
-    ?>
-    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                <div class="caramel-slide <?php if($i == 1){echo('caramel-slide--active');} ?>">
+                    <div class="caramel-slide__number"><?php echo($i); ?></div>
+                    <?php
+                        the_post_thumbnail('', array('class' => 'caramel-slide__image'));
+                    ?>
+                    <div class="caramel-slide__text"><?php echo(get_the_title($post)); ?></div>
+                </div>
+                <?php
+                $i++;
+            }
+        ?>
+    </div>
+    <a id="caramel-slider-prev" class="caramel-slider__controller caramel-slider__controller--prev">&#10094;</a>
+    <a id="caramel-slider-next" class="caramel-slider__controller caramel-slider__controller--next">&#10095;</a>
 </div>
-<div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-</div>
+
+
+
 <div class="home-banner">
 </div>
 
