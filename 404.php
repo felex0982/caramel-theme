c21c2e3b91c75c889a4cd5d813aa0906f471eb47
Felix <felix@bottlebase.com>
2020-07-31 12:02:08 +0200

    new 404 page

--- 404.php
+++ 404.php
@@ -12,78 +12,15 @@ get_header();
 
 ?>
 
-<div class="wrapper" id="error-404-wrapper">
+<div class="caramel-single-wrapper" id="single-wrapper">
 
-	<div class="" id="content" tabindex="-1">
+	<div class="caramel-single-content" id="content" tabindex="-1">
 
-		<div class="row">
+		<h1 class="caramel-entry-title"><?php esc_html_e( '404: Something went wrong...', 'caramel' ); ?></h1>
 
-			<div class="col-md-12 content-area" id="primary">
+	</div>
 
-				<main class="site-main" id="main">
-
-					<section class="error-404 not-found">
-
-						<header class="page-header">
-
-							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'caramel' ); ?></h1>
-
-						</header><!-- .page-header -->
-
-						<div class="page-content">
-
-							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'caramel' ); ?></p>
-
-							<?php get_search_form(); ?>
-
-							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-							<?php if ( understrap_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-
-								<div class="widget widget_categories">
-
-									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'caramel' ); ?></h2>
-
-									<ul>
-										<?php
-										wp_list_categories(
-											array(
-												'orderby'  => 'count',
-												'order'    => 'DESC',
-												'show_count' => 1,
-												'title_li' => '',
-												'number'   => 10,
-											)
-										);
-										?>
-									</ul>
-
-								</div><!-- .widget -->
-
-							<?php endif; ?>
-
-							<?php
-
-							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'caramel' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-							the_widget( 'WP_Widget_Tag_Cloud' );
-							?>
-
-						</div><!-- .page-content -->
-
-					</section><!-- .error-404 -->
-
-				</main><!-- #main -->
-
-			</div><!-- #primary -->
-
-		</div><!-- .row -->
-
-	</div><!-- #content -->
-
-</div><!-- #error-404-wrapper -->
+</div>
 
 <?php
 get_footer();
